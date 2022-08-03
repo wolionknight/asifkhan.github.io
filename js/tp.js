2e490e62f9be9fabb917a88778feacc814fea311
Asif-The-Wolion <>
2022-08-03 20:49:30 +0600

    updated tp.js

--- js/tp.js
+++ js/tp.js
@@ -1,18 +1,11 @@
-var script = document.createElement('script');
-    script.type = 'text/javascript';
-
-    script.src = '';
-    document.body.appendChild(script);
-
   function sendEmail(){
   Email.send({
     Host : "smtp.elasticemail.com",
     Username : "thewolion@gmail.com",
     Password : "0596F5153760161CCB9E511A2AD5BC9ADF50",
     To : 'thewolion@gmail.com',
-    BCC: document.getElementById("email").value,
     From : document.getElementById("email").value,
-    Subject : "New messenge from Asif Khan Website",
+    Subject : "New messenge"+document.getElementById("name").value,
     Body : "name:"+document.getElementById("name").value
     + "</br> email: "+document.getElementById("email").value
     + "</br> phone: "+document.getElementById("phonenumber").value
